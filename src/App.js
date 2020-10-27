1c3d4786146b9643d7021aa0943fe193dab98a26
Travis Southard <tsouthard88@gmail.com>
2020-10-27 18:01:16 -0400

    Set out basics for a react-based garden

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React, {useState} from "react"
 import './App.css';
 
 function App() {
+  const [plots, setplots] = useState([])
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Travis' Garden</h1>
+      {plots.map((plot, index) => {
+        return (<article>
+          <h3>{plot.title}</h3>
+        </article>)
+      })}
+    </>
   );
 }
 
