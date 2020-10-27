6d61182da58946791cb7930f3d7c3c0020afdcb4
Travis Southard <tsouthard88@gmail.com>
2020-10-27 18:28:36 -0400

    React app now talks to backend and can fetch data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 import './App.css';
 
-function App() {
-  const [plots, setplots] = useState([])
+let baseUrl;
+if (process.env.NODE_ENV === 'development') {
+  baseUrl = 'http://localhost:3003';
+} else {
+  baseUrl = 'https://xpense-backend.herokuapp.com';
+}
+console.log('current base URL:', baseUrl);
 
-  
+function App() {
+  const [plots, setPlots] = useState([])
+
+  let getGarden = () => {
+    fetch(baseUrl + "/garden/").then(res => {
+      return res.json()
+    }).then(data => {
+      setPlots(data)
+    })
+  }
+
+  useEffect(() => {
+    getGarden()
+  })
 
   return (
     <>
       <h1>Travis' Garden</h1>
       {plots.map((plot, index) => {
-        return (<article>
+        return (<article key={index}>
           <h3>{plot.title}</h3>
         </article>)
       })}
