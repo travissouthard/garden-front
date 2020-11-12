2fc866c540be94783ad17cd04f552f7683040fe0
Travis Southard <tsouthard88@gmail.com>
2020-11-12 10:37:50 -0500

    Added a functions folder for reusable functions and put in our display datetime function

--- src/components/Plot.jsx
+++ src/components/Plot.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Post from "./Post"
 import PostForm from "./PostForm"
+import displayDatetime from "../functions/displayDatetime"
 
 export default class Plot extends Component {
     displayTags = (array) => {
@@ -8,26 +9,26 @@ export default class Plot extends Component {
         return tags
     }
 
-    displayDatetime = (datetime) => {
-        let date = datetime.slice(0, 10)
-        let time = datetime.slice(11, 19)
-        let dateArray = date.split("-")
-        let timeArray = time.split(":")
+    // displayDatetime = (datetime) => {
+    //     let date = datetime.slice(0, 10)
+    //     let time = datetime.slice(11, 19)
+    //     let dateArray = date.split("-")
+    //     let timeArray = time.split(":")
 
-        let readableDate = dateArray[1] + "/" + dateArray[2] + "/" + dateArray[0]
-        if (timeArray[0] == "00") {
-            timeArray[0] = "12"
-            timeArray[2] = "AM"
-        } else if (parseInt(timeArray[0]) > 12) {
-            timeArray[0] = (parseInt(timeArray[0]) - 12).toString()
-            timeArray[2] = "PM"
-        } else {
-            timeArray[2] = "AM"
-        }
-        timeArray[1] = ":" + timeArray[1] + " "
+    //     let readableDate = dateArray[1] + "/" + dateArray[2] + "/" + dateArray[0]
+    //     if (timeArray[0] == "00") {
+    //         timeArray[0] = "12"
+    //         timeArray[2] = "AM"
+    //     } else if (parseInt(timeArray[0]) > 12) {
+    //         timeArray[0] = (parseInt(timeArray[0]) - 12).toString()
+    //         timeArray[2] = "PM"
+    //     } else {
+    //         timeArray[2] = "AM"
+    //     }
+    //     timeArray[1] = ":" + timeArray[1] + " "
 
-        return readableDate + ", " + timeArray.join("")
-    }
+    //     return readableDate + ", " + timeArray.join("")
+    // }
     
     render() {
         return (
@@ -42,7 +43,7 @@ export default class Plot extends Component {
                     setPostTitle={this.props.setPostTitle}
                     setDescription={this.props.setDescription}
                 />
-                <p>Last Updated: {this.displayDatetime(this.props.plot.updatedAt)}</p>
+                <p>Last updated: {displayDatetime(this.props.plot.updatedAt)}</p>
                 <p>{this.displayTags(this.props.plot.tags)}</p>
             </article>
         )
---
+++ src/functions/displayDatetime.js
@@ -0,0 +1,22 @@
+displayDatetime = (datetime) => {
+    let date = datetime.slice(0, 10)
+    let time = datetime.slice(11, 19)
+    let dateArray = date.split("-")
+    let timeArray = time.split(":")
+
+    let readableDate = dateArray[1] + "/" + dateArray[2] + "/" + dateArray[0]
+    if (timeArray[0] == "00") {
+        timeArray[0] = "12"
+        timeArray[2] = "AM"
+    } else if (parseInt(timeArray[0]) > 12) {
+        timeArray[0] = (parseInt(timeArray[0]) - 12).toString()
+        timeArray[2] = "PM"
+    } else {
+        timeArray[2] = "AM"
+    }
+    timeArray[1] = ":" + timeArray[1] + " "
+
+    return readableDate + ", " + timeArray.join("")
+}
+
+export {displayDatetime}
