3ae74ccd512ec231a612bd759e336e7e5561b545
Travis Southard <tsouthard88@gmail.com>
2020-12-27 18:51:14 -0500

    Made the new post form toggleable, but while the boolean changes correctly, the ternary operator to display or hide tghe form only shows the false option and there is a bug that adds new PostForm components to the components tree

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,31 +1,47 @@
 import React, { Component } from 'react'
 
 export default class PostForm extends Component {
+    state = {
+        formOn: false
+    }
+
+    toggleForm(event) {
+        event.preventDefault()
+        this.setState({
+            formOn: !this.state.formOn
+        })
+    }
+    
     render() {
         return (
             <form onSubmit={(event) => this.props.handleSubmit(event, this.props.title)}>
-                <fieldset>
-                    <legend>{this.props.title}</legend>
-                    <label htmlFor="postTitle">Title:
-                        <input
-                            type="text" 
-                            id="postTitle" 
-                            name="form" 
-                            value={this.props.postTitle} 
-                            onChange={(event) => this.props.handleChange(event, this.props.setPostTitle)}
-                        />
-                    </label>
-                    <label htmlFor="description">Description:
-                        <textarea
-                            type="text" 
-                            id="description" 
-                            name="form"
-                            onChange={(event) => this.props.handleChange(event, this.props.setDescription)}
-                        >{this.props.description}</textarea>
-                            
-                    </label>
-                    <input type="submit" value="Add post"/>
-                </fieldset>
+                {this.formOn === true ? (
+                    <fieldset>
+                        <legend>{this.props.title}</legend>
+                        <label htmlFor="postTitle">Title:
+                            <input
+                                type="text" 
+                                id="postTitle" 
+                                name="form" 
+                                value={this.props.postTitle} 
+                                onChange={(event) => this.props.handleChange(event, this.props.setPostTitle)}
+                            />
+                        </label>
+                        <label htmlFor="description">Description:
+                            <textarea
+                                type="text" 
+                                id="description" 
+                                name="form"
+                                onChange={(event) => this.props.handleChange(event, this.props.setDescription)}
+                            >{this.props.description}</textarea>
+                                
+                        </label>
+                        <input type="submit" value="Add post"/>
+                        <button onClick={(event) => this.toggleForm(event)}>Cancel</button>
+                    </fieldset>
+                ) : (
+                    <button onClick={(event) => this.toggleForm(event)}>Add post</button>
+                )}
             </form>
         )
     }
